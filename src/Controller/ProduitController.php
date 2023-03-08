587562d8e87d3843db350d373fe18a3fafa98b4e
younesali0 <ali.younes1@esprit.tn>
2023-03-08 14:08:32 +0100

    metier

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Seancecoaching;
+use Dompdf\Options;
+use Dompdf\Dompdf;
+
 use App\Entity\Produit;
 use App\Form\ProduitType;
 use App\Repository\ProduitRepository;
@@ -11,19 +15,46 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Validator\Constraints\Json;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use phpDocumentor\Reflection\DocBlock\Serializer;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Knp\Component\Pager\PaginatorInterface;
+
+
 
 
 
 #[Route('/produit')]
 class ProduitController extends AbstractController
 {
-    #[Route('/', name: 'app_produit_index', methods: ['GET'])]
+    #[Route('/showback', name: 'app_produit_indexx', methods: ['GET'])]
     public function index(ProduitRepository $produitRepository): Response
     {
         return $this->render('produit/index.html.twig', [
             'produits' => $produitRepository->findAll(),
         ]);
     }
+    #[Route('/show', name: 'app_produit_index', methods: ['GET'])]
+        public function indexe(Request $request , ProduitRepository $produitRepository,PaginatorInterface $paginator): Response
+        {   $produits=$produitRepository->findAll() ;
+            $pagination = $paginator->paginate(
+                $produits,
+                $request->query->getInt('page', 1),
+                3// items per page
+            );
+            return $this->render('produit/showfront.html.twig', [
+                'produits' => $pagination ,
+            ]);
+        }
 
     #[Route('/new', name: 'app_produit_new', methods: ['GET', 'POST'])]
     public function new(Request $request, ProduitRepository $produitRepository, SluggerInterface $slugger): Response
@@ -68,10 +99,17 @@ class ProduitController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_produit_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_produit_showb', methods: ['GET'])]
     public function show(Produit $produit): Response
     {
         return $this->render('produit/show.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
+    #[Route('/front/{id}', name: 'app_produit_showf', methods: ['GET'])]
+    public function showf(Produit $produit): Response
+    {
+        return $this->render('produit/showfront.html.twig', [
             'produit' => $produit,
         ]);
     }
@@ -109,7 +147,7 @@ class ProduitController extends AbstractController
             
             $produitRepository->save($produit, true);
 
-            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_produit_indexx', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('produit/edit.html.twig', [
@@ -125,6 +163,76 @@ class ProduitController extends AbstractController
             $produitRepository->remove($produit, true);
         }
 
-        return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_produit_indexx', [], Response::HTTP_SEE_OTHER);
     }
+
+    #[Route('/pdf/listpdf', name: 'app_produit_liste', methods: ['GET'])]
+    public function listpdfproduit(EntityManagerInterface $entityManager): Response
+    {    // Configure Dompdf according to your needs
+        $pdfOptions = $dompdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        $produits = $entityManager
+            ->getRepository(Produit::class)
+            ->findAll();
+
+
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('produit/listpdfproduit.html.twig', [
+            'produits' => $produits,
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("productlistpdf.pdf", [
+            "Attachment" => true
+        ]);
+    }
+
+    /**
+     * @Route("/produit/pdf", name="produit_pdf")
+     */
+    public function generatePdf(): Response
+    {
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        $dompdf = new Dompdf($pdfOptions);
+
+        $produits = $this->getDoctrine()->getRepository(Produit::class)->findAll();
+
+        $html = $this->renderView('produit/pdf_template.html.twig', [
+            'produits' => $produits,
+        ]);
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+
+        $dompdf->render();
+
+        $pdf = $dompdf->output();
+
+        return new Response($pdf, 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="Produit.pdf"',
+        ]);
+    }
+
+
+
+
 }
+
+
